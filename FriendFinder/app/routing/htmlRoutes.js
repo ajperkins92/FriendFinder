ef5bbed44b05c506c4a291fbf0810dcc7b8ec56a
Alex Perkins <alex.jakob.perkins@gmail.com>
2019-11-23 00:39:53 -0800

    Refigures server.js and the routing folders

--- FriendFinder/app/routing/htmlRoutes.js
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -1,30 +1,13 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
 var path = require("path");
 
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
-module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+module.exports = function (app) {
+  app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname + "/../public/survey.html"));
+    console.log("Successfully connected to survey.");
   });
 
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  app.use(function (res, res) {
+    res.sendFile(path.join(__dirname + "/../public/home.html"));
+    console.log("Successfully connected to home.");
   });
-
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-};
+}
--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,29 +1,16 @@
-// ==============================================================================
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
-
 var express = require("express");
+var path = require("path");
 var app = express();
-
-// Tells node that we are creating an "express" server
-var app = express();
-
-// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
+var app = express();
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-
-
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
