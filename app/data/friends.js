3a2569e35dc8341deb46f5667207d109ed1abe96
Alex Perkins <alex.jakob.perkins@gmail.com>
2020-01-29 22:25:43 -0800

    Adds photos to friendslist API

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,27 +3,27 @@
 var friends = [
     {
         name: "Cliff",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        photo: "https://image.shutterstock.com/image-photo/image-250nw-575234476.jpg",
         scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
     },
     {
         name: "Frank",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        photo: "https://media.gettyimages.com/photos/portrait-of-smiling-young-man-wearing-eyeglasses-picture-id985138634?s=612x612",
         scores: ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2"]
     },
     {
         name: "Lee",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        photo: "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
     },
     {
         name: "Patrick",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        photo: "https://image.shutterstock.com/image-photo/portrait-middle-aged-man-260nw-556638379.jpg",
         scores: ["4", "4", "4", "4", "4", "4", "4", "4", "4", "4"]
     },
     {
         name: "Jess",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        photo: "https://media.istockphoto.com/photos/young-longhaired-smiling-woman-in-white-shirt-picture-id965523228?k=6&m=965523228&s=612x612&w=0&h=qeVmQfjRq1QWxaLdxLdF_IaXahI-dqt9UYcunaHUqA4=",
         scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
     }];
 
